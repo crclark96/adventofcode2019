21a1064c9450fdd8e8b6030eb4297ca731bec1c5
Collin Reilly Clark <collin.clark@jhuapl.edu>
2019-12-04 07:59:22 -0500

    advent day 04 - first star

---
+++ advent04-1.hs
@@ -0,0 +1,10 @@
+match :: Int -> Bool
+match i = len && inc && adj
+  where len   = length str == 6
+        inc   = foldl (\acc x -> fst x <= snd x && acc) True  pairs
+        adj   = foldl (\acc x -> fst x == snd x || acc) False pairs
+        str   = show i
+        pairs = zip str $ tail str
+
+main = do
+  print $ length . filter (\x -> x) $ map match [272091..815432]
