67fa34602efce00a635bf9691c5dc034ac32a668
Collin Reilly Clark <crclark96@gmail.com>
2019-12-03 22:01:03 -0500

    make day 02 faster - with maps!
    change implementation because the replace function was carrying a lot
    of overhead. also: lazy maps are faster (slightly) but also take more space

--- advent02-1.hs
+++ advent02-1.hs
@@ -1,21 +1,17 @@
-replace :: Int -> a -> [a] -> [a]
-replace _ _ [] = []
-replace i n (x:xs)
-  | i == 0 = n:xs
-  | otherwise = x:replace (i-1) n xs
+import Data.Map.Lazy as M
 
-runop :: Int -> [Int] -> [Int]
+runop :: Int -> M.Map Int Int -> M.Map Int Int
 runop i list
   | opcode == 99 = list
-  | opcode == 1 = runop (i+4) $ replace dst (src1 + src2) list
-  | opcode == 2 = runop (i+4) $ replace dst (src1 * src2) list
-    where opcode = list !! i
-          src1   = list !! (list !! (i+1))
-          src2   = list !! (list !! (i+2))
-          dst    = list !! (i+3)
+  | opcode == 1 = runop (i+4) $ M.insert dst (src1 + src2) list
+  | opcode == 2 = runop (i+4) $ M.insert dst (src1 * src2) list
+    where opcode = list ! i
+          src1   = list ! (list ! (i+1))
+          src2   = list ! (list ! (i+2))
+          dst    = list ! (i+3)
 
 main = do
   contents <- getContents
-  let input = map (read :: String -> Int) $ words [if x == ',' then ' ' else x | x <- contents]
-      state = replace 1 12 $ replace 2 2 input
-  print $ (runop 0 state) !! 0
+  let input = M.fromList $ zip [0..] $ Prelude.map (read :: String -> Int) $ words [if x == ',' then ' ' else x | x <- contents]
+      state = M.insert 1 12 $ M.insert 2 2 input
+  print $ (runop 0 state) ! 0
--- advent02-2.hs
+++ advent02-2.hs
@@ -1,28 +1,24 @@
-search :: Int -> Int -> [Int] -> Int
+import Data.Map.Lazy as M
+
+search :: Int -> Int -> Map Int Int -> Int
 search noun verb state
   | noun < 0 = search 99 (verb-1) state
   | result == 19690720 = 100 * noun + verb
   | otherwise = search (noun-1) verb state
-    where result = (runop 0 $ replace 1 noun $ replace 2 verb state) !! 0
+    where result = (runop 0 $ M.insert 1 noun $ M.insert 2 verb state) ! 0
 
-replace :: Int -> a -> [a] -> [a]
-replace _ _ [] = []
-replace i n (x:xs)
-  | i == 0 = n:xs
-  | otherwise = x:replace (i-1) n xs
-
-runop :: Int -> [Int] -> [Int]
+runop :: Int -> M.Map Int Int -> M.Map Int Int
 runop i list
   | opcode == 99 = list
-  | opcode == 1 = runop (i+4) $ replace dst (src1 + src2) list
-  | opcode == 2 = runop (i+4) $ replace dst (src1 * src2) list
-    where opcode = list !! i
-          src1   = list !! (list !! (i+1))
-          src2   = list !! (list !! (i+2))
-          dst    = list !! (i+3)
+  | opcode == 1 = runop (i+4) $ M.insert dst (src1 + src2) list
+  | opcode == 2 = runop (i+4) $ M.insert dst (src1 * src2) list
+    where opcode = list ! i
+          src1   = list ! (list ! (i+1))
+          src2   = list ! (list ! (i+2))
+          dst    = list ! (i+3)
 
 main = do
   contents <- getContents
-  let input = map (read :: String -> Int) $ words [if x == ',' then ' ' else x | x <- contents]
-      state = replace 1 12 $ replace 2 2 input
+  let input = M.fromList $ zip [0..] $ Prelude.map (read :: String -> Int) $ words [if x == ',' then ' ' else x | x <- contents]
+      state = M.insert 1 12 $ M.insert 2 2 input
   print $ search 99 99 state
