004b1ee820dc172c9b579e8d4aad561d1e3111f3
Collin Reilly Clark <collin.clark@jhuapl.edu>
2019-12-10 13:28:10 -0500

    advent day 10 - second star
    hardest part of this one was getting the representation right with the inverted y axis
    and the lazer sweeping from the 'top' (negative y axis) and also the bounds on the angles
    for each quadrant (does a==pi/2 belong in q1 or q2?)

--- advent10-2.hs
+++ advent10-2.hs
@@ -1,4 +1,5 @@
 import qualified Data.Set as S
+import qualified Data.Map.Lazy as M
 
 type Coords = (Int, Int)
 type Grid = S.Set Coords
@@ -16,30 +17,38 @@ createGrid' x y m g
           w  = length (m !! 0)
 
 angle :: Coords -> Coords -> Float
-angle (x1,y1) (x2,y2) = atan (y/x) + q
+angle (x1,y1) (x2,y2) = if a < 0 then a + 2*pi else a
   where x = fromIntegral $ x2 - x1
-        y = fromIntegral $ y2 - y1
-        q = case (x,y) of _
-                           | (x <= 0 && y >= 0) ->  pi
-                           | (x <= 0 && y <= 0) -> -pi
-                           | otherwise          ->   0
+        y = fromIntegral $ y1 - y2
+        a = atan2 y x
 
 numVisible :: Coords -> Grid -> Int
 numVisible c g = S.size . S.map (\x -> angle c x) $ S.delete c g
 
-visible :: Coords -> Grid -> S.Set (Coords, Float)
-visible c g = S.map (\x -> (x, angle c x)) $ S.delete c g
+visible :: Coords -> Grid -> Grid
+visible c g = S.fromList $ M.elems m
+  where m  = M.fromListWith (\c1 c2 -> if dist c1 c < dist c2 c then c1 else c2) $ S.elems $ S.map (\x -> (angle c x,x)) $ S.delete c g
+        -- map (angle:coords)
+
+dist :: Coords -> Coords -> Float
+dist (x1,y1) (x2,y2) = abs $ sqrt (x^2 + y^2)
+  where x = fromIntegral $ x2 - x1
+        y = fromIntegral $ y2 - y1
 
 main = do
   input <- readFile "input/input10"
   let m = lines input
       g = createGrid m
       station = fst $ foldr1 (\x acc -> if snd x > snd acc then x else acc) $ S.map (\x -> (x, numVisible x g)) g
-      angles  = S.map (\x -> (x, angle station x)) $ S.delete station g
-      q1 = S.filter (\(c,a) -> 0<a && a<(pi/2)) angles
-      q2 = S.filter (\(c,a) -> (-pi/2)<a && a<0) angles
-      q3 = S.filter (\(c,a) -> (-pi)<a && a<(-pi/2)) angles
-      q4 = S.filter (\(c,a) -> (pi/2)<a && a<pi) angles
-      n  = 200 - S.size q1 - S.size q2 - 1
-  print $ (S.toDescList $ S.map (\(c,a) -> (a,c)) q3) !! n
-
+      v = visible station g
+      angles  = S.map (\x -> (angle station x,x)) v
+      q1 = S.filter (\(a,c) -> 0<a && a<=(pi/2)) angles
+      q2 = S.filter (\(a,c) -> (pi/2)<a && a<=pi) angles
+      q3 = S.filter (\(a,c) -> pi<a && a<=(3/2)*pi) angles
+      q4 = S.filter (\(a,c) -> (3/2)*pi<a || a==0) angles
+      n  = 200 - S.size q1 - S.size q4 - S.size q3 - 1
+  print $ S.size q1
+  print $ S.size q2
+  print $ S.size q3
+  print $ S.size q4
+  print $ (S.toDescList q2) !! n
